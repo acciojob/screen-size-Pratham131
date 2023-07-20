07eeac88c19a85216c0a4c4030910a65dbe5cb50
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-20 09:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Function to update the size information in the <h1> tag
+function updateSizeInfo() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const sizeInfoElement = document.querySelector("#sizeInfo h1");
+    sizeInfoElement.textContent = `Width: ${width} and Height: ${height}`;
+}
+
+// Call the updateSizeInfo function when the window is resized
+window.addEventListener('resize', updateSizeInfo);
+
+// Initial call to set the initial size information
+updateSizeInfo();
